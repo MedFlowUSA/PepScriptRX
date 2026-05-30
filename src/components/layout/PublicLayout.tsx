2b06b2428387174eea497fbbc1084e008b363a08
MedFlowUSA <stemsellsllc@gmail.com>
2026-05-30 00:36:51 -0700

    Add rep intake to quick links

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -210,6 +210,7 @@ export default function PublicLayout({
                 <div className="pub-footer-links">
                   <Link to="/" className="pub-footer-link">Home</Link>
                   <Link to="/start" className="pub-footer-link">Start Refill Request</Link>
+                  <Link to="/rep-intake" className="pub-footer-link">Rep Store Setup</Link>
                   <Link to="/library" className="pub-footer-link">Compound Library</Link>
                   <Link to="/peptide-calculator" className="pub-footer-link">PrecisionMix Calculator</Link>
                   <Link to="/login" className="pub-footer-link">Customer Login</Link>
